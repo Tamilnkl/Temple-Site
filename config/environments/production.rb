3d1cf8f444d6607c6d0fbf948fa4fb3a79def842
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2015-03-18 22:29:51 +0530

    se changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'pkv_kavadi',
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
+  }
 end
