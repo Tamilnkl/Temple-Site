cf97a31da4851cf5069c5a06b8392709b80662c0
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2015-03-18 23:19:57 +0530

    rails admin enum conflicts resolved

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,13 @@ class Image < ActiveRecord::Base
 	has_attached_file :pics, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :pics, :content_type => /\Aimage\/.*\Z/
 
-    enum image_type: ["God","Sivanmalai","Person"]
+    IMAGETYPE = ["God","Sivanmalai","Person"]
+    enum image_type: IMAGETYPE
 
     validates :image_type,presence: true
     validates :pics, attachment_presence: true
+
+    def self.image_type_enum
+    	IMAGETYPE.each_with_index.to_a
+  	end
 end
