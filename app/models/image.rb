f6233dbe76a71ca02ff0b542d31215f231ec8291
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2015-03-19 19:52:33 +0530

    image gallery

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-	has_attached_file :pics, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :pics, :styles => { :medium => "300x300>", :thumb => "100x100>", :big => "1351x900!" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :pics, :content_type => /\Aimage\/.*\Z/
 
     IMAGETYPE = ["God","Sivanmalai","Person"]
@@ -10,5 +10,9 @@ class Image < ActiveRecord::Base
 
     def self.image_type_enum
     	IMAGETYPE.each_with_index.to_a
+  	end
+
+  	def big_image
+  		pics.url(:big)
   	end
 end
