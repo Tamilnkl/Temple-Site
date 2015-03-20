d29f7e3c75dd56dcb69ba02bf43a816ce4eaf604
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2015-03-20 14:54:19 +0530

    changes

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
-	has_many :images
-	belongs_to :image
+	# has_many :images
+	# belongs_to :image
 
 	has_attached_file :pics, :styles => { :medium => "300x300>", :thumb => "100x100>", :big => "1351x900!" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :pics, :content_type => /\Aimage\/.*\Z/
