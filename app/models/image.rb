40d0fe2a254f3d81f01a8adc7e1c5d5b77f097f6
Dhanabal <dhanabal@Dhanabal-Vostro-3560.(none)>
2015-03-23 00:03:27 +0530

    gallery and sub gallery

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,10 +2,11 @@ class Image < ActiveRecord::Base
   has_many :images
   belongs_to :image
 
-	has_attached_file :pics, :styles => { :medium => "300x300>", :thumb => "100x100>", :big => "1351x900!" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :pics, :styles => { :medium => "450x293!", :thumb => "721x658!", :big => "1351x900!" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :pics, :content_type => /\Aimage\/.*\Z/
 
   scope :eligible_parent_image, ->(image_type_id) {where(image_type: image_type_id)}
+  scope :parent_image, -> {where("image_id is null")}
 
   IMAGETYPE = ["god","sivanmalai","person"]
   enum image_type: IMAGETYPE
@@ -17,13 +18,13 @@ class Image < ActiveRecord::Base
   	IMAGETYPE.each_with_index.to_a
 	end
 
-	def big_image
-		pics.url(:big)
-	end
+  def sized_image(size)
+    pics.url(size.to_sym)
+  end
 
   def self.eligible_parent(*options)
     image_type_id = []
     options.each{|type| image_type_id << IMAGETYPE.index(type)}
-    eligible_parent_image(image_type_id)
+    eligible_parent_image(image_type_id).parent_image
   end
 end
